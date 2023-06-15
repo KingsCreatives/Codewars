896321a03f5da7be1cd63b64fafea088849d72f8
Kwame Cody <kwamecody@gmail.com>
2023-06-15 09:29:17 +0000

    refactored code

--- 8kyu/superEasy.js
+++ 8kyu/superEasy.js
@@ -1,9 +1,13 @@
-function problem(x){
-    let value;
-    //your code here
-    if(typeof(x) !== "number"){
-      return "Error"
-    } else{
-      return x * 50 + 6
-    }
-  }
+// function problem(x){
+//     let value;
+//     //your code here
+//     if(typeof(x) !== "number"){
+//       return "Error"
+//     } else{
+//       return x * 50 + 6
+//     }
+//   }
+
+  let problem = (x) => typeof(x) !== "number" ? "Error" : x * 50 + 6; 
+
+  
