9172746a1c274756d967ad181020683c83119b3c
Kwame Cody <kwamecody@gmail.com>
2023-06-23 20:17:02 +0000

    kata solution

---
+++ 8kyu/convert-bool-to-str.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function boolToString(b){
+    return b.toString()
+}
