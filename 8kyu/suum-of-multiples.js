bea190387bdf4572de674e5b2cabf0b0f19d4110
Kwame Cody <kwamecody@gmail.com>
2023-07-22 05:59:52 +0000

    kata solution

---
+++ 8kyu/suum-of-multiples.js
@@ -0,0 +1,14 @@
+function sumMul(n, m) {
+    //your idea here
+    if (n <= 0 || m <= 0) {
+        return "INVALID"
+    }
+    let sum = 0
+
+    for (let i = n; i < m; i++) {
+        if (i % n === 0) {
+            sum += i;
+        }
+    }
+    return sum
+}
