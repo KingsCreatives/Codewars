fb459f04dff1eb984af151f77bbb14a95559ef8b
Kwame Cody <kwamecody@gmail.com>
2023-08-03 17:39:26 +0000

    kata solution

---
+++ 7kyu/bongo.js
@@ -0,0 +1,8 @@
+function bingo(a) {
+    // your winning code here
+    if(a.includes(2) && a.includes(9) && a.includes(14) && a.includes(7) && a.includes(15)){
+          return "WIN"
+      }else{
+          return "LOSE"
+      }
+  }
