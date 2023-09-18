d55ebd6a640fc599b386a8fd37e552b88d697699
Kwame Cody <kwamecody@gmail.com>
2023-09-18 15:27:24 +0000

    Deleted leetcode solution

--- leetcode/easy/lenOfLastWord.js
+++
@@ -1,6 +0,0 @@
-// leeetcode - lenOfLastSTr
-var lengthOfLastWord = function(s) {
-    s = s.trim().split(' ')
-    let lastItem = s[s.length -1]
-    return lastItem.length;
-};
--- leetcode/easy/palindrone.js
+++
@@ -1,7 +0,0 @@
-var isPalindrome = function(s) {
-    s = s.toLowerCase()
-    const regex = /[^a-zA-Z0-9]/g;
-    const cleanStr = s.replace(regex, "")
-
-   return cleanStr.split("").join("") === cleanStr.split("").reverse().join("")
-};
