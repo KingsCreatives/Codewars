ac30a443ddf8405a6102dc0b7c028426bb859da9
Kwame Cody <kwamecody@gmail.com>
2023-09-28 18:54:06 +0000

    kata solution

---
+++ 7kyu/countDigits.js
@@ -0,0 +1,7 @@
+function countDigits(n) {
+    // Convert the integer to a string
+    const str = n.toString();
+  
+    // Return the length of the string, which is the number of digits
+    return str.length;
+  }
