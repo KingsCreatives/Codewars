1f5058407f2455a372e9c1956a9b074f3e3da89c
Kwame Cody <kwamecody@gmail.com>
2023-10-02 02:38:06 -0700

    commented code

--- 6kyu/multiplicationTable.js
+++ 6kyu/multiplicationTable.js
@@ -1,12 +1,29 @@
 function multiplicationTable(n) {
+    // Array to store the result
     let result = [];
+
+    // Loop through each row
     for (let i = 1; i <= n; i++) {
+        // Array to store the current row
         let row = [];
+
+        // Loop through each column
         for (let j = 1; j <= n; j++) {
+            // Calculate the product and add it to the row
             row.push(i * j);
         }
+
+        // Add the row to the result
         result.push(row);
     }
 
+    // Return the final multiplication table
     return result;
 }
+
+
+
+
+
+
+
