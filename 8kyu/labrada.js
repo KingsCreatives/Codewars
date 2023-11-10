0be8ff5368f02e9e117612970df1969b34d7fe65
kwame cody <kwamecody@gmail.com>
2023-11-10 15:49:40 +0000

    kata solution

---
+++ 8kyu/labrada.js
@@ -0,0 +1,9 @@
+class Labrador extends Dog {
+    constructor(name, age, gender, master) {
+      super(name,age,gender, "Labrador","Large", 4,true, master)
+      this.name = name
+      this.age = age
+      this.gender = gender
+      this.master = master
+    }
+  }
