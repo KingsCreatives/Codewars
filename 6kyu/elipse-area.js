d3491e394304ebac8457f2f14a851461cb10cf05
Kwame Cody <kwamecody@gmail.com>
2024-03-05 15:52:25 +0000

    kata solution

---
+++ 6kyu/elipse-area.js
@@ -0,0 +1,19 @@
+/*
+Based on the received dimensions, a and b, of an ellipse, calculare its area and perimeter.
+
+Example:
+Input: elipse(5,2)
+
+Output: "Area: 31.4, perimeter: 23.1"
+Note: The perimeter approximation formula you should use: π * (3/2(a+b) - sqrt(ab))
+ */
+
+// solution
+function elipse(a,b){
+    let area = Math.PI * a * b
+    let perimeter = Math.PI * (3/2 * (a + b) - Math.sqrt(a * b))
+
+    return `Area: ${area.toFixed(1)}, perimeter: ${perimeter.toFixed(1)}`
+}
+
+console.log(elipse(5,2))
