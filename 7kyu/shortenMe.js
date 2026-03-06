20cad3788a7207a18d3444824c9c2c999362e410
kwamecody <kwamecody@gmail.com>
2026-03-06 06:29:04 +0000

    solution to kata

---
+++ 7kyu/shortenMe.js
@@ -0,0 +1,9 @@
+/**
+ * You have to write an array() function returning an array object with elements as follow:
+
+[ 'u', 'n', 'd', 'e', 'f', 'i', 'n', 'e', 'd' ]
+Coding Limitation:
+Less than 25 characters
+ */
+
+array = () => [...([][0] + '')];
