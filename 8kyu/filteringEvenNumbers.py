dcff211512493573ecb0fc6a71cd7424bc5bb19e
kwamecody <kwamecody@gmail.com>
2026-03-09 06:43:35 +0000

    fixed code to filter even numbers

---
+++ 8kyu/filteringEvenNumbers.py
@@ -0,0 +1,6 @@
+#The method is supposed to remove even numbers from the list and return a list that contains the odd numbers.
+
+# However, there is a bug in the method that needs to be resolved.
+
+def kata_13_december(lst):
+    return [x for x in lst if x % 2 != 0]
