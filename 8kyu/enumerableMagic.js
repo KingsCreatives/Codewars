ef92c27028b1dce572f387dfcbb3e01e875439e9
kwamecody <kwamecody@gmail.com>
2026-04-10 23:14:07 +0000

    kata solution

---
+++ 8kyu/enumerableMagic.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function that accepts a sequence and a non-negative integer n, and returns a sequence of the first n elements from it.
+
+If the sequence has fewer than n elements, return all of them.
+
+Examples
+[0, 1, 2, 3, 5, 8, 13], n = 3  =>  [0, 1, 2]
+[0, 1, 2, 3, 5, 8, 13], n = 0  =>  []
+                    [], n = 3  =>  []
+If you need help, here's a reference:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+ */
