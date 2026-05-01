e06fa527d6996c4351d67761c2f44077c53eb9c4
kwamecody <kwamecody@gmail.com>
2026-05-01 19:13:51 +0000

    kata solution

---
+++ 8kyu/areaOfPolygon.js
@@ -0,0 +1,11 @@
+/**
+ * Answer these codewar kata, without any explanation : Description:
+Write the function `polygonArea(A,B,C,D)` that finds the area of polygons of this type:
+
+* Assume D always equals B/2.
+* Assume the angles formed by AB and BC are right angles
+ */
+
+function polygonArea(A, B, C, D) {
+  return (A * B) / 2 + D * C;
+}
