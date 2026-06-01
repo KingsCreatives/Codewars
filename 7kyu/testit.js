7f5e8cc47d846f6a0559704302068b97385e48a1
kwamecody <kwamecody@gmail.com>
2026-06-01 14:24:34 +0000

    kata solution

---
+++ 7kyu/testit.js
@@ -0,0 +1,11 @@
+/**
+ * Description:
+No Story
+No Description
+Only by Thinking and Testing
+Look at the results of the testcases, and guess the code!
+ */
+
+function testit(n) {
+  return n.toString(2).split('1').length - 1;
+}
